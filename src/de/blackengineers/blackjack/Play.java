655d7c6b82f6a87ccfffac13fcded5275af49026
Michael Obst <michael.obst@telekom.de>
2019-11-27 11:15:14 +0100

    Package hinzugefügt

---
+++ src/de/blackengineers/blackjack/Play.java
@@ -0,0 +1,19 @@
+package de.blackengineers.blackjack;
+
+
+public class Play {
+
+
+
+	public static void main(String[] args) {
+		
+		// initialisiere Deck
+		
+		// starteSpiel (verteile Karten)
+		
+		// Schleife bis gewonnen 
+			// spiele eine Runde
+
+	}
+
+}
