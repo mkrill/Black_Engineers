46f910c55f82222fcc17ac4439d47efd24b49912
Michael Obst <michael.obst@telekom.de>
2019-11-27 16:51:14 +0100

    Klasse Play kommentiert und Textausgabe formatiert

--- src/de/blackengineers/blackjack/Play.java
+++ src/de/blackengineers/blackjack/Play.java
@@ -7,25 +7,30 @@ public class Play {
 	private Player thePlayer;
 	private Dealer theDealer;
 	private Deck myDeck;
+
 	
+	// initialisiere einen statischen Scanner
 	static public Scanner ourScanner = new Scanner(System.in);
+
 	
-	// privater Kontruktor
+	// privater Kontruktor, um eine Play-Klasse zu instanziieren
 	private Play() {
 		this.thePlayer = new Player();
 		this.theDealer = new Dealer();
-		
 	}
+
 	
+	// main-Methode instanziiert Play() und ruft play() auf
 	public static void main(String[] args) {
 		new Play().play();
 	}
-
 	
 	
 	private void play() {
 		String eingabe;
-		// Schleife 1 Spiel spielen, bis "leave"
+		System.out.println("\n*** Los geht's!! ***\n");
+
+		//solange spielen bis der Spieler kein (j/J) eingibt
 		boolean wantsToPlay = true;
 		do {
 
@@ -38,17 +43,14 @@ public class Play {
 			thePlayer.takeFirstTwoCards(myDeck);
 			theDealer.takeFirstTwoCards(myDeck);
 			
+			// Spieler spielt zu Ende
 			thePlayer.finalizeRound(myDeck);
-			
 			int resultPlayer = thePlayer.getHandValue();
-			// int resultPlayer = thePlayer.playRound(myDeck); //Alternative Participant.playRound() liefert direkt den Kartenwert zurück
 
 			// nur wenn Spieler <= 21 hat, spielt der Dealer
-
 			if (resultPlayer <= 21) {
 				theDealer.finalizeRound(myDeck);
 				int resultDealer = theDealer.getHandValue();
-				//int resultDealer = theDealer.playRound(myDeck); //Alternative, siehe oben
 				
 				
 				if (resultDealer > 21) {
@@ -66,21 +68,19 @@ public class Play {
 			
 			
 
-			// Abfrage weiter spielen?
-			System.out.println("Möchtest Du ein neues Spiel spielen? (j/n) ");
+			// Abfrage weiterspielen?
+			System.out.println("\nMöchtest Du ein neues Spiel spielen? (j/n) ");
 			eingabe = ourScanner.next();
 			wantsToPlay=eingabe.equalsIgnoreCase("j");
 			if (wantsToPlay) {
-				System.out.println("\n\n\n");
+				System.out.println("\n*** Neues Spiel, neues Glück!! ***\n");
 			}
 		}
 		while (wantsToPlay);
 			
-		System.out.println("Es war mir eine große Freude! Auf Wiedersehen!");
+		System.out.println("\n*** Es war mir eine große Freude! Auf Wiedersehen! ***");
 		
 		ourScanner.close();
-
-
 	}
 
 
